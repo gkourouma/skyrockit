b8d735e16963582cdb4d6ee32b298a3f78ad79ae
gkourouma <gkourouma2@gmail.com>
2025-03-12 13:54:03 -0500

    wip: required and mounted passUseerToView and IsSignedIn

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const morgan = require('morgan');
 const session = require('express-session');
 
 const authController = require('./controllers/auth.js');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -28,6 +30,8 @@ app.use(
   })
 );
 
+app.use(passUserToView); //has to be placed after session middleware annd before homepage
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
@@ -35,6 +39,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authController);
+app.use(isSignedIn); // placed after auth routes- user need to authenticate first
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
