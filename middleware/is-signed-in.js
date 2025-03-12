c0d33a71639924be1082107503ddc218f909ae38
gkourouma <gkourouma2@gmail.com>
2025-03-12 15:38:14 -0500

    added comments to middleware function

--- middleware/is-signed-in.js
+++ middleware/is-signed-in.js
@@ -1,7 +1,7 @@
 // middleware/is-signed-in.js
 
 const isSignedIn = (req, res, next) => {
-    if (req.session.user) return next();
+    if (req.session.user) return next();//check if user is logged in and go to next middleware or route
     res.redirect('/auth/sign-in');
   };
   
