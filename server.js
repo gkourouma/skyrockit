d754fc0b11d64b49c0ca7bcd999440a371dfbbfa
gkourouma <gkourouma2@gmail.com>
2025-03-12 15:37:05 -0500

    fixed formatting

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-const session = require('express-session');
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const session = require("express-session");
 
-const authController = require('./controllers/auth.js');
-const isSignedIn = require('./middleware/is-signed-in.js');
-const passUserToView = require('./middleware/pass-user-to-view.js');
+const authController = require("./controllers/auth.js");
+const applicationsController = require('./controllers/application.js')
 
-const port = process.env.PORT ? process.env.PORT : '3000';
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
+
+const port = process.env.PORT ? process.env.PORT : "3000";
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
+mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
-app.use(morgan('dev'));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -31,14 +33,21 @@ app.use(
 );
 
 app.use(passUserToView); //has to be placed after session middleware annd before homepage
+app.use('/users/:userId/applications', applicationsController);
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
+
+app.get("/", (req, res) => {
+//check if user is signed in
+if(req.session.user){
+// redirecting signed in users to their application
+  res.redirect(`/users/${req.session.user._id}/applications`)
+} else {
+  // show the homepage for users who are not signed in
+  res.render('index.ejs')
+}
 });
 
-app.use('/auth', authController);
+app.use("/auth", authController);
 app.use(isSignedIn); // placed after auth routes- user need to authenticate first
 
 app.listen(port, () => {
