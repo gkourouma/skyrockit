860ca9bca4f4ebee841a700a3e99720b5d2a7a62
gkourouma <gkourouma2@gmail.com>
2025-03-12 13:18:08 -0500

    wip: add application schema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,29 @@
+
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    requires: true
+  },
+  title: {
+    type: String,
+    requires: true
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted']
+  },
+})
+
+
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,6 +32,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications:[applicationSchema]
 });
 
 const User = mongoose.model('User', userSchema);
